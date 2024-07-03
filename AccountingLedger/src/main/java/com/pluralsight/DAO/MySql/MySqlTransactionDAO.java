e4b4b7b4eff9c6bcb84602db9d61ea9a51d8001a
angelica <angelicaolvera490@gmail.com>
2024-07-03 16:20:58 -0700

    Resolved merge conflicts and fixed errors in MySqlTransactionTypeDAO

--- AccountingLedger/src/main/java/com/pluralsight/DAO/MySql/MySqlTransactionDAO.java
+++ AccountingLedger/src/main/java/com/pluralsight/DAO/MySql/MySqlTransactionDAO.java
@@ -1,49 +1,28 @@
 package com.pluralsight.DAO.MySql;
 import com.pluralsight.DAO.TransactionDAO;
 import com.pluralsight.models.Transaction;
-<<<<<<< HEAD
-
-=======
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
->>>>>>> 1e841120e8280170ac2792aff3266143b53172f5
 import java.math.BigDecimal;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
-<<<<<<< HEAD
-public class MySqlTransactionDAO implements TransactionDAO {
-    private final Connection connection;
-
-    public MySqlTransactionDAO(Connection connection) {
-        this.connection = connection;
-    }
-
-    @Override
-    public void addTransaction(Transaction transaction) {
-        String sql = "INSERT INTO Transactions (customer_id, amount, transaction_date) VALUES (?, ?, ?)";
-        try (PreparedStatement statement = connection.prepareStatement(sql)) {
-=======
 @Component
 public class MySqlTransactionDAO extends MySQLDaoBase implements TransactionDAO {
 
     @Autowired
     public MySqlTransactionDAO(DataSource dataSource) {
         super(dataSource);
-
     }
-
 
     @Override
     public void addTransaction(Transaction transaction) {
-
         try (Connection connection = getConnection()) {
             String sql = "INSERT INTO Transactions (customer_id, amount, transaction_date) VALUES (?, ?, ?)";
             PreparedStatement statement = connection.prepareStatement(sql);
->>>>>>> 1e841120e8280170ac2792aff3266143b53172f5
             statement.setInt(1, transaction.getCustomerId());
             statement.setBigDecimal(2, transaction.getAmount());
             statement.setTimestamp(3, Timestamp.valueOf(transaction.getTransactionDate()));
@@ -55,15 +34,9 @@ public class MySqlTransactionDAO extends MySQLDaoBase implements TransactionDAO 
 
     @Override
     public Transaction getTransaction(int id) {
-<<<<<<< HEAD
-        String sql = "SELECT * FROM Transactions WHERE transaction_id = ?";
-        try (PreparedStatement statement = connection.prepareStatement(sql)) {
-=======
-
         try (Connection connection = getConnection()) {
             String sql = "SELECT * FROM Transactions WHERE transaction_id = ?";
             PreparedStatement statement = connection.prepareStatement(sql);
->>>>>>> 1e841120e8280170ac2792aff3266143b53172f5
             statement.setInt(1, id);
             ResultSet resultSet = statement.executeQuery();
             if (resultSet.next()) {
@@ -78,16 +51,10 @@ public class MySqlTransactionDAO extends MySQLDaoBase implements TransactionDAO 
     @Override
     public List<Transaction> getAllTransactions() {
         List<Transaction> transactions = new ArrayList<>();
-<<<<<<< HEAD
-        String sql = "SELECT * FROM Transactions";
-        try (Statement statement = connection.createStatement();
-             ResultSet resultSet = statement.executeQuery(sql)) {
-=======
         try (Connection connection = getConnection()) {
             String sql = "SELECT * FROM Transactions";
             Statement statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery(sql);
->>>>>>> 1e841120e8280170ac2792aff3266143b53172f5
             while (resultSet.next()) {
                 transactions.add(mapRow(resultSet));
             }
@@ -99,14 +66,9 @@ public class MySqlTransactionDAO extends MySQLDaoBase implements TransactionDAO 
 
     @Override
     public void updateTransaction(Transaction transaction) {
-<<<<<<< HEAD
-        String sql = "UPDATE Transactions SET customer_id = ?, amount = ?, transaction_date = ? WHERE transaction_id = ?";
-        try (PreparedStatement statement = connection.prepareStatement(sql)) {
-=======
         try (Connection connection = getConnection()) {
             String sql = "UPDATE Transactions SET customer_id = ?, amount = ?, transaction_date = ? WHERE transaction_id = ?";
             PreparedStatement statement = connection.prepareStatement(sql);
->>>>>>> 1e841120e8280170ac2792aff3266143b53172f5
             statement.setInt(1, transaction.getCustomerId());
             statement.setBigDecimal(2, transaction.getAmount());
             statement.setTimestamp(3, Timestamp.valueOf(transaction.getTransactionDate()));
@@ -119,15 +81,9 @@ public class MySqlTransactionDAO extends MySQLDaoBase implements TransactionDAO 
 
     @Override
     public void deleteTransaction(int id) {
-<<<<<<< HEAD
-        String sql = "DELETE FROM Transactions WHERE transaction_id = ?";
-        try (PreparedStatement statement = connection.prepareStatement(sql)) {
-=======
-
         try (Connection connection = getConnection()) {
             String sql = "DELETE FROM Transactions WHERE transaction_id = ?";
             PreparedStatement statement = connection.prepareStatement(sql);
->>>>>>> 1e841120e8280170ac2792aff3266143b53172f5
             statement.setInt(1, id);
             statement.executeUpdate();
         } catch (SQLException e) {
--- AccountingLedger/src/main/java/com/pluralsight/DAO/MySql/MySqlTransactionTypeDAO.java
+++ AccountingLedger/src/main/java/com/pluralsight/DAO/MySql/MySqlTransactionTypeDAO.java
@@ -1,18 +1,6 @@
 package com.pluralsight.DAO.MySql;
-
 import com.pluralsight.DAO.TransactionTypeDAO;
 import com.pluralsight.models.TransactionType;
-<<<<<<< HEAD
-import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
-
-public class MySqlTransactionTypeDAO implements TransactionTypeDAO {
-    private final Connection connection;
-
-    public MySqlTransactionTypeDAO(Connection connection) {
-        this.connection = connection;
-=======
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -20,26 +8,20 @@ import javax.sql.DataSource;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
+
 @Component
 public class MySqlTransactionTypeDAO extends MySQLDaoBase implements TransactionTypeDAO {
-    private DataSource dataSource;
-@Autowired
+
+    @Autowired
     public MySqlTransactionTypeDAO(DataSource dataSource) {
         super(dataSource);
->>>>>>> 1e841120e8280170ac2792aff3266143b53172f5
     }
 
     @Override
     public void addTransactionType(TransactionType transactionType) {
-<<<<<<< HEAD
         String sql = "INSERT INTO TransactionTypes (type) VALUES (?)";
-        try (PreparedStatement statement = connection.prepareStatement(sql)) {
-=======
-
-        try (Connection connection = getConnection()) {
-            String sql = "INSERT INTO TransactionTypes (type) VALUES (?)";
-            PreparedStatement statement = connection.prepareStatement(sql);
->>>>>>> 1e841120e8280170ac2792aff3266143b53172f5
+        try (Connection connection = getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
             statement.setString(1, transactionType.getType());
             statement.executeUpdate();
         } catch (SQLException e) {
@@ -49,15 +31,9 @@ public class MySqlTransactionTypeDAO extends MySQLDaoBase implements Transaction
 
     @Override
     public TransactionType getTransactionType(int id) {
-<<<<<<< HEAD
         String sql = "SELECT * FROM TransactionTypes WHERE transaction_type_id = ?";
-        try (PreparedStatement statement = connection.prepareStatement(sql)) {
-=======
-
-        try (Connection connection = getConnection()) {
-            String sql = "SELECT * FROM TransactionTypes WHERE transaction_type_id = ?";
-            PreparedStatement statement = connection.prepareStatement(sql);
->>>>>>> 1e841120e8280170ac2792aff3266143b53172f5
+        try (Connection connection = getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
             statement.setInt(1, id);
             ResultSet resultSet = statement.executeQuery();
             if (resultSet.next()) {
@@ -72,16 +48,10 @@ public class MySqlTransactionTypeDAO extends MySQLDaoBase implements Transaction
     @Override
     public List<TransactionType> getAllTransactionTypes() {
         List<TransactionType> transactionTypes = new ArrayList<>();
-<<<<<<< HEAD
         String sql = "SELECT * FROM TransactionTypes";
-        try (Statement statement = connection.createStatement();
+        try (Connection connection = getConnection();
+             Statement statement = connection.createStatement();
              ResultSet resultSet = statement.executeQuery(sql)) {
-=======
-        try (Connection connection = getConnection()) {
-            String sql = "SELECT * FROM TransactionTypes";
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery(sql);
->>>>>>> 1e841120e8280170ac2792aff3266143b53172f5
             while (resultSet.next()) {
                 transactionTypes.add(mapRow(resultSet));
             }
@@ -93,15 +63,9 @@ public class MySqlTransactionTypeDAO extends MySQLDaoBase implements Transaction
 
     @Override
     public void updateTransactionType(TransactionType transactionType) {
-<<<<<<< HEAD
         String sql = "UPDATE TransactionTypes SET type = ? WHERE transaction_type_id = ?";
-        try (PreparedStatement statement = connection.prepareStatement(sql)) {
-=======
-
-        try (Connection connection = getConnection()) {
-            String sql = "UPDATE TransactionTypes SET type = ? WHERE transaction_type_id = ?";
-            PreparedStatement statement = connection.prepareStatement(sql);
->>>>>>> 1e841120e8280170ac2792aff3266143b53172f5
+        try (Connection connection = getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
             statement.setString(1, transactionType.getType());
             statement.setInt(2, transactionType.getId());
             statement.executeUpdate();
@@ -112,15 +76,9 @@ public class MySqlTransactionTypeDAO extends MySQLDaoBase implements Transaction
 
     @Override
     public void deleteTransactionType(int id) {
-<<<<<<< HEAD
         String sql = "DELETE FROM TransactionTypes WHERE transaction_type_id = ?";
-        try (PreparedStatement statement = connection.prepareStatement(sql)) {
-=======
-
-        try (Connection connection = getConnection()) {
-            String sql = "DELETE FROM TransactionTypes WHERE transaction_type_id = ?";
-            PreparedStatement statement = connection.prepareStatement(sql);
->>>>>>> 1e841120e8280170ac2792aff3266143b53172f5
+        try (Connection connection = getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
             statement.setInt(1, id);
             statement.executeUpdate();
         } catch (SQLException e) {
