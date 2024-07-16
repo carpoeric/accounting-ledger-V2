b142b7b54d9ac434f6149850844ae81a592b789e
Agatha Jung <theagathajungg@gmail.com>
2024-07-16 15:05:20 -0700

    fixed project structure

--- AccountingLedger/src/main/java/com/pluralsight/Configuration/DatabaseConfig.java
+++ AccountingLedger/src/main/java/com/pluralsight/Configuration/DatabaseConfig.java
@@ -6,27 +6,23 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-    @Configuration
-    public class DatabaseConfig
-    {
-        private BasicDataSource basicDataSource;
+@Configuration
+public class DatabaseConfig {
 
-        @Bean
-        public BasicDataSource dataSource()
-        {
-            return basicDataSource;
-        }
+    private BasicDataSource basicDataSource;
 
-        @Autowired
-        public DatabaseConfig(@Value("${datasource.url}") String url,
-                              @Value("${datasource.username}") String username,
-                              @Value("${datasource.password}") String password)
-        {
-            basicDataSource = new BasicDataSource();
-            basicDataSource.setUrl(url);
-            basicDataSource.setUsername(username);
-            basicDataSource.setPassword(password);
-        }
-
+    @Bean
+    public BasicDataSource dataSource() {
+        return basicDataSource;
     }
 
+    @Autowired
+    public DatabaseConfig(@Value("${spring.datasource.url}") String url,
+                          @Value("${spring.datasource.username}") String username,
+                          @Value("${spring.datasource.password}") String password) {
+        basicDataSource = new BasicDataSource();
+        basicDataSource.setUrl(url);
+        basicDataSource.setUsername(username);
+        basicDataSource.setPassword(password);
+    }
+}
