05a4d8392020c8269855ffa8f2a8b7fbe59ff0ed
Agatha Jung <theagathajungg@gmail.com>
2024-07-17 15:27:31 -0700

    fixed webconfig

--- AccountingLedger/src/main/java/com/pluralsight/Configuration/WebConfig.java
+++ AccountingLedger/src/main/java/com/pluralsight/Configuration/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")  // Adjust this as needed for your frontend URL
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
