25b2c26b7fb3368583f4e1eda2f98a03b16bcdf9
Agatha Jung <theagathajungg@gmail.com>
2024-07-17 12:02:55 -0700

    updated java files

--- AccountingLedger/src/main/java/com/pluralsight/Controllers/ReportsController.java
+++ AccountingLedger/src/main/java/com/pluralsight/Controllers/ReportsController.java
@@ -3,10 +3,10 @@ package com.pluralsight.Controllers;
 import com.pluralsight.DAO.TransactionDAO;
 import com.pluralsight.models.Transaction;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -15,6 +15,7 @@ import java.time.*;
 
 @RestController
 @CrossOrigin
+@EnableMethodSecurity
 @RequestMapping("reports")
 
 public class ReportsController {
@@ -26,14 +27,16 @@ public class ReportsController {
     }
 
     @GetMapping("/monthToDate")
+    @ResponseStatus(HttpStatus.OK)
     public List<Transaction> monthToDate(){
         LocalDateTime today = LocalDateTime.now();
         return transactionDao.getAllTransactions().stream()
                 .filter(t-> t.getTransactionDate().getMonthValue() == today.getMonthValue())
                 .toList();
     }
-
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/previousMonths")
+    @ResponseStatus(HttpStatus.OK)
     public List<Transaction> getPreviousMonths(){
         LocalDateTime today = LocalDateTime.now();
         return transactionDao.getAllTransactions().stream()
@@ -42,14 +45,24 @@ public class ReportsController {
     }
 
     @GetMapping("/yearToDate")
+    @ResponseStatus(HttpStatus.OK)
     public List<Transaction> yearToDate(){
         LocalDateTime today = LocalDateTime.now();
         return transactionDao.getAllTransactions().stream()
-                .filter(t-> t.getTransactionDate().getYear() < today.getYear())
+                .filter(t-> t.getTransactionDate().getYear() == today.getYear())
                 .toList();
     }
 
     // add version of yearToDate with path variable for given year
+
+    @GetMapping("/{year}")
+    public List<Transaction> getByYear(@PathVariable int year){
+        LocalDateTime today = LocalDateTime.now();
+        List <Transaction> transactions = transactionDao.getAllTransactions().stream()
+                .filter(t-> t.getTransactionDate().getYear() == year)
+                .toList();
+        return transactions;
+    }
 
     // we don't have a field for vendor in any of the models, perhaps we can add vendor into a new table, may result in moving these methods elsewhere.
 //    public List<Transaction> searchByVendor(String Vendor){
